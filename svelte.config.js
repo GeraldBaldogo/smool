19d9114d8e0a7653e68f3c2dc7579c6238505d98
Gerald Baldogo <baldogogerald@gmail.com>
2026-03-20 18:34:53 +0800

    pin compatible vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter()
-  }
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
